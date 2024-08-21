f88bb8c5c31512faf5246c5d442d60c8b7af18a8
giriraj-dev <prajapatigiriraj7@gmail.com>
2024-08-21 18:26:15 +0530

    basic

--- main.js
+++ main.js
@@ -1,24 +1,33 @@
 let string = "";
 let buttons = document.querySelectorAll("button");
+
+const operators = new Map ([["+",true],["-",true],["*",true],["/",true],["%",true]]);
+
+function isLastCharOperator(){
+  const lastIndex  = string.length-1;
+  const lastValue = string[lastIndex]
+
+  return operators.has(lastValue)
+}
+
 Array.from(buttons).forEach((button) => {
   button.addEventListener("click", (e) => {
+      
     if (e.target.innerHTML == "=") {
       string = eval(string);
       document.querySelector("input").value = string;
-     string="";
-    } 
-    else if (e.target.innerHTML == "C") {
+      string = "";
+    }
+      else if (e.target.innerHTML == "C") {
       string = "";
       document.querySelector("input").value = string;
     } else if (e.target.innerHTML == "+/-") {
       string = string * -1;
       document.querySelector("input").value = string;
-    }
-      else if(e.target.innerHTML == "Del"){
-        string= string.slice(0,-1);
-        document.querySelector("input").value = string;
-      }
-    else {
+    } else if (e.target.innerHTML == "Del") {
+      string = string.slice(0, -1);
+      document.querySelector("input").value = string;
+    } else {
       console.log(e.target);
       string = string + e.target.innerHTML;
       document.querySelector("input").value = string;
@@ -26,5 +35,29 @@ Array.from(buttons).forEach((button) => {
   });
 });
 
+ // let lastDigit = string.split("")[string.length - 1];
+
+//const arthOperators = ["+", "%", "-", "/", "*"];
+
+// else if(!arthOperators.includes(lastDigit))        // if arthmatic symbol is not placed then this if block will work
+//         {
+//             string = string + e.target.innerHTML;
+//             document.querySelector("input").value = string;
+//     }
+
+// const isOperator = (value) =>{
+//     return arthOperators.includes(lastDigit)
+// }
 
 
+
+// if operator --> don't take anything
+// else if(not operator) --> then appen new value
+
+    // } else if (
+    //   string.split("")[string.length - 1] == "+" ||
+    //   string.split("")[string.length - 1] == "%" ||
+    //   string.split("")[string.length - 1] == "-" ||
+    //   string.split("")[string.length - 1] == "*" ||
+    //   string.split("")[string.length - 1] == "/"
+    // ) 
